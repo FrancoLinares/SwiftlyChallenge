1b679c49bdfc40de409f5faa1f7617fb90e7a1f3
Franco Linares <franco.linares@gmail.com>
2024-04-21 19:43:51 -0300

    feat: add query provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import Logo from '@/components/Logo';
-import ErrorBoundary from './ErrorBoundary';
+import ErrorBoundary from '@/ErrorBoundary';
 import Search from '@/components/Search';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <ErrorBoundary>
-      <div className="mx-auto max-w-9xl sm:px-7 lg:px-8 h-full">
-        <header className="flex flex-row items-center py-8">
-          <Logo />
-        </header>
-        <div className="relative isolate overflow-hidden px-6 text-center sm:px-16">
-          <h1 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Didn't find Star Wars character you were looking for?
-          </h1>
-          <Search />
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary>
+        <div className="mx-auto max-w-9xl sm:px-7 lg:px-8 h-full">
+          <header className="flex flex-row items-center py-8">
+            <Logo />
+          </header>
+          <div className="relative isolate overflow-hidden px-6 text-center sm:px-16">
+            <h1 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Didn't find Star Wars character you were looking for?
+            </h1>
+            <Search />
+          </div>
         </div>
-      </div>
-    </ErrorBoundary>
+      </ErrorBoundary>
+    </QueryClientProvider>
   );
 }
 
