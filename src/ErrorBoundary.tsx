8668d8bd73e03a1e34722724e4b8f1307aa64fe3
Franco Linares <franco.linares@gmail.com>
2024-04-21 19:34:52 -0300

    feat: add Error Boundary component

---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,53 @@
+import React, { Component, ReactNode } from 'react';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: object): void {
+    // Log the error to an error reporting service
+    console.error('Error caught:', error, errorInfo);
+  }
+
+  render(): ReactNode {
+    if (this.state.hasError) {
+      return (
+        <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+          <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+            <div className="relative">
+              <div className="absolute">
+                <div className="">
+                  <h1 className="my-2 text-gray-800 font-bold text-2xl">
+                    Looks like something went wrong
+                  </h1>
+                  <p className="my-2 text-gray-800">Sorry about that!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img src="https://i.ibb.co/ck1SGFJ/Group.png" />
+          </div>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
