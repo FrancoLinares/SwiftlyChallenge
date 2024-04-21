fe49dd7a250ea034426d26d4d1cc56a670ff5658
Franco Linares <franco.linares@gmail.com>
2024-04-21 19:55:27 -0300

    feat: add Pagination component

---
+++ src/components/UI/Pagination/Pages.tsx
@@ -0,0 +1,25 @@
+import { PaginationProps } from '.';
+
+const Pages = ({
+  currentPage,
+  allCharactersCount
+}: {
+  currentPage: PaginationProps['currentPage'];
+  allCharactersCount: PaginationProps['allCharactersCount'];
+}) => {
+  return (
+    <div>
+      <p className="text-sm text-gray-700">
+        Showing <span className="font-medium">{currentPage * 10 + 1}</span> to{' '}
+        <span className="font-medium">
+          {(currentPage + 1) * 10 >= allCharactersCount
+            ? allCharactersCount
+            : (currentPage + 1) * 10}
+        </span>{' '}
+        of <span className="font-medium">{allCharactersCount}</span> results
+      </p>
+    </div>
+  );
+};
+
+export default Pages;
---
+++ src/components/UI/Pagination/index.tsx
@@ -0,0 +1,73 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid';
+import { NEXT, PREVIOUS } from './constants';
+import Pages from './Pages';
+
+export type PaginationProps = {
+  currentPage: number;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+  fetchNextPage: () => void;
+  isFetchingNextPage: boolean;
+  hasNextPage: boolean;
+  allPagesCount: number;
+  allCharactersCount: number;
+};
+
+const Pagination = ({
+  currentPage,
+  setCurrentPage,
+  fetchNextPage,
+  isFetchingNextPage,
+  hasNextPage,
+  allPagesCount,
+  allCharactersCount
+}: PaginationProps) => {
+  return (
+    <footer className="fixed bottom-0 left-0 right-0">
+      <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+          <Pages
+            currentPage={currentPage}
+            allCharactersCount={allCharactersCount}
+          />
+          <div>
+            <nav
+              className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+              aria-label="Pagination"
+            >
+              <button
+                disabled={currentPage === 0}
+                className={`relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 ${
+                  currentPage === 0 && 'opacity-25'
+                }`}
+                onClick={() => {
+                  setCurrentPage(currentPage - 1);
+                }}
+              >
+                <span className="sr-only">{PREVIOUS}</span>
+                <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+              </button>
+              <button
+                disabled={!hasNextPage || isFetchingNextPage}
+                className={`relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 ${
+                  !hasNextPage || (isFetchingNextPage && 'opacity-25')
+                }`}
+                onClick={() => {
+                  // The next page has not yet been populated
+                  if (allPagesCount <= currentPage + 1) {
+                    fetchNextPage();
+                  }
+                  setCurrentPage(currentPage + 1);
+                }}
+              >
+                <span className="sr-only">{NEXT}</span>
+                <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+              </button>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Pagination;
